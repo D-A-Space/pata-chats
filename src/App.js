696da1bcd3281fcca478b6630263e5134eb07969
AZZIE2000 <azzam.faraj@gmail.com>
2023-04-15 02:27:36 +0300

    done logic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,97 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/analytics";
+import uuid from "react-uuid";
+import React, { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/analytics';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-firebase.initializeApp({
-    apiKey: "AIzaSyBPpaph0g52dKIHCveIExcPecx5V99ce34",
+const appy = firebase.initializeApp({
+  apiKey: "AIzaSyBPpaph0g52dKIHCveIExcPecx5V99ce34",
   authDomain: "pata-chats.firebaseapp.com",
   projectId: "pata-chats",
   storageBucket: "pata-chats.appspot.com",
   messagingSenderId: "305329005180",
-  appId: "1:305329005180:web:204f10e5581ce01fac1ff9"
-})
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-const analytics = firebase.analytics();
+  appId: "1:305329005180:web:204f10e5581ce01fac1ff9",
+});
+// TODO : icons api
+// https://www.dicebear.com/styles/fun-emoji
+const db = getFirestore(appy);
 function App() {
-    const [user] = useAuthState(auth);
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState("");
+  const [userId, setUserId] = useState(localStorage.getItem("userId") || "");
+  // const user = firebase.auth().currentUser;
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt"),
+      limit(50)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe;
+  }, []);
+  useEffect(() => {
+    const userUid =
+      localStorage.getItem("userId") || localStorage.setItem("userId", uuid());
+    setUserId(userUid);
+  }, []);
+  const sendMessage = async () => {
+    const message = {
+      text: text,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: userId,
+    };
+    await addDoc(collection(db, "messages"), {
+      ...message,
+    });
+    setText("");
+  };
 
   return (
     <div className="App">
-   <div className="text-5xl text-red-500 text-center">hi</div>
+      {!userId === "" || messages.length > 0 ? (
+        messages.map((message) => (
+          <div key={message.id}>
+            <h3 className={`${message.uid === userId ? "text-red-600" : "text-black"}`}>{message.text}</h3>
+          </div>
+        ))
+      ) : (
+        <div>
+          <h3>loading</h3>
+        </div>
+      )}
+
+    
+      <div className="mt-7 flex ">
+        <input
+          value={text}
+          className="border-2"
+          type="text"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />{" "}
+        <button onClick={() => sendMessage()} className="p-3 bg-red-500">
+          SEND
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
